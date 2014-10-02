3dd80e7f4cae30ca42bb0f1a87a3c8e936537e49
sanromd <damiansra@gmail.com>
2014-10-02 17:21:56 +0300

    maxwell.py now hasn an example for off and pulse shapes, source and boundary conditions are set accordingly

--- maxwell_vc_2d/maxwell.py
+++ maxwell_vc_2d/maxwell.py
@@ -5,37 +5,51 @@ import numpy as np
 sys.path.append(os.path.realpath('../utils'))
 sys.path.append(os.path.realpath('../'))
 
-
 from utils.materials import Material2D
 from utils.sources import Source2D
 
 x_lower = 0.0
-x_upper = 10.0
+x_upper = 50.0
 
 y_lower = 0.0
-y_upper = 10.0
+y_upper = 50.0
+
+sy = y_upper-y_lower
+sx = x_upper-x_lower
 
 material = Material2D(shape='homogeneous',metal=False)
 material.setup()
 material._calculate_n()
 
-source = Source2D(material,shape='pulse',wavelength=2.0)
-source.offset[1] = -5.0
-source.setup()
-
 def grid_basic(x_lower,x_upper,y_lower,y_upper,mx,my,cfl):
     dx = (x_upper-x_lower)/mx
     dy = (y_upper-y_lower)/my
     dt = 0.90/(material.co*np.sqrt(1.0/(dx**2)+1.0/(dy**2)))
-    tf = (x_upper-x_lower+5.0)/material.v
-    
+    tf = 2.0*(x_upper-x_lower+5.0)/material.v
+
     return dx,dy,dt,tf
 
-def em2D(mx=2**11,my=2**6,num_frames=100,cfl=1.0,outdir='./_output',before_step=True,debug=False):
+def em2D(mx=128,my=128,num_frames=10,cfl=1.0,outdir='./_output',before_step=False,debug=False,heading='x',shape='off'):
     import clawpack.petclaw as pyclaw
     import petsc4py.PETSc as MPI
 
-    if MPI.COMM_WORLD.rank==0:
+    source = Source2D(material,shape=shape,wavelength=2.0)
+
+    if shape=='off':
+        source.offset.fill(5.0)
+        if heading=='xy':
+            source.offset[0] = sy/2.0
+            source.offset[1] = sx/2.0
+    else:
+        source.offset.fill(-5.0)
+        source.transversal_offset = sy/2.0
+        source.transversal_width = sy
+        source.transversal_shape = 'cosine'
+
+    source.heading = heading
+    source.setup()
+
+    if (debug and MPI.COMM_WORLD.rank==0):
         material.dump()
         source.dump()
 
@@ -48,7 +62,7 @@ def em2D(mx=2**11,my=2**6,num_frames=100,cfl=1.0,outdir='./_output',before_step=
 
     x = pyclaw.Dimension('x',x_lower,x_upper,mx)
     y = pyclaw.Dimension('y',y_lower,y_upper,my)
-    
+
     domain = pyclaw.Domain([x,y])
 
 #   Solver settings
@@ -61,34 +75,38 @@ def em2D(mx=2**11,my=2**6,num_frames=100,cfl=1.0,outdir='./_output',before_step=
     solver.dt_initial  = dt/2.0
     solver.dt_max      = dt
     solver.max_steps   = int(2*tf/dt)
-    
+
 #   Import Riemann and Tfluct solvers
     import maxwell_2d_rp
     import maxwell_2d_tfluct
 
     solver.tfluct_solver = True
     solver.fwave = True
-    
+
     solver.rp = maxwell_2d_rp
     solver.tfluct = maxwell_2d_tfluct
-    
+
     solver.cfl_max = cfl+0.5
     solver.cfl_desired = cfl
+    solver.reflect_index = [1,0]
 
 #   boundary conditions
-    solver.bc_lower[0] = pyclaw.BC.custom
-    solver.bc_lower[1] = pyclaw.BC.extrap
-    solver.bc_upper[0] = pyclaw.BC.extrap
-    solver.bc_upper[1] = pyclaw.BC.extrap
-    solver.user_bc_lower = source.scattering_bc
+    if shape=='off':
+        solver.bc_lower[0] = pyclaw.BC.wall
+        solver.aux_bc_lower[0]= pyclaw.BC.wall
+    else:
+        solver.bc_lower[0] = pyclaw.BC.custom
+        solver.aux_bc_lower[0]= pyclaw.BC.custom
+        solver.user_bc_lower = source.scattering_bc
+        solver.user_aux_bc_lower = material.setaux_lower
 
-    solver.aux_bc_lower[0]= pyclaw.BC.custom
-    solver.aux_bc_lower[1]= pyclaw.BC.extrap
-    solver.aux_bc_upper[0]= pyclaw.BC.extrap
-    solver.aux_bc_upper[1]= pyclaw.BC.extrap
-    
-    solver.user_aux_bc_lower = material.setaux_lower
-    solver.user_aux_bc_upper = material.setaux_upper
+    solver.bc_lower[1] = pyclaw.BC.wall
+    solver.bc_upper[0] = pyclaw.BC.wall
+    solver.bc_upper[1] = pyclaw.BC.wall
+
+    solver.aux_bc_lower[1]= pyclaw.BC.wall
+    solver.aux_bc_upper[0]= pyclaw.BC.wall
+    solver.aux_bc_upper[1]= pyclaw.BC.wall
 
 #   before step configure
     if before_step:
@@ -97,7 +115,7 @@ def em2D(mx=2**11,my=2**6,num_frames=100,cfl=1.0,outdir='./_output',before_step=
 
 #   state setup
     state = pyclaw.State(domain,num_eqn,num_aux)
-    
+
     state.problem_data['chi2']  = material.chi2
     state.problem_data['chi3']  = material.chi3
     state.problem_data['vac1']  = material.eo
