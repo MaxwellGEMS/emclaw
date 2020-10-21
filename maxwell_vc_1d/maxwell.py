f2bf8feef51dd1f90db87d093c785d8814e214c2
damiansa <damiansra@gmail.com>
2020-10-21 10:26:27 +0300

    updated maxwell 1D routine to correctly use petsc and make it compatible with latest pyclaw release

--- maxwell_vc_1d/maxwell.py
+++ maxwell_vc_1d/maxwell.py
@@ -25,10 +25,28 @@ def grid_basic(x_lower,x_upper,mx,cfl):
 
     return dx,dt,tf
 
-def em1D(mx=1024,num_frames=10,cfl=1.0,outdir='./_output',before_step=False,debug=False,chi3=0.0,chi2=0.0,nl=False,psi=True,em=True,conservative=True):
+def set_outdirs(outdir, debug):
+    material._outdir = outdir
+    source._outdir   = outdir
+    if debug: 
+        material._dump_to_latex()
+        source._dump_to_latex()
 
-    import clawpack.petclaw as pyclaw
-    import petsc4py.PETSc as MPI
+def em1D(mx = 1024, num_frames = 10, use_petsc = True, reconstruction_order = 5, lim_type = 2,  cfl = 1.0, conservative = True,
+         chi3 = 0.0, chi2 = 0.0, nl = False, psi = True, em = True, before_step = False,
+         debug = False, outdir = './_output', output_style = 1):
+
+    if use_petsc:
+        import clawpack.petclaw as pyclaw
+    else:
+        from clawpack import pyclaw
+
+    try:
+        import petsc4py.PETSc as MPI
+        parallel = True
+    except:
+        print('petsc4py not available')
+        parallel = False
 
     if nl:
         material.chi3_e = chi3
@@ -37,11 +55,10 @@ def em1D(mx=1024,num_frames=10,cfl=1.0,outdir='./_output',before_step=False,debu
             material.chi3_m = chi3
             material.chi2_m = chi2
 
-    if MPI.COMM_WORLD.rank==0:
-        material._outdir = outdir
-        source._outdir   = outdir
-        material._dump_to_latex()
-        source._dump_to_latex()
+    if np.logical_and(parallel, MPI.COMM_WORLD.rank==0):
+        set_outdirs(outdir, debug)
+    else:
+        set_outdirs(outdir, debug)
 
     num_eqn   = 2
     num_waves = 2
@@ -56,7 +73,8 @@ def em1D(mx=1024,num_frames=10,cfl=1.0,outdir='./_output',before_step=False,debu
     solver=pyclaw.SharpClawSolver1D()
     solver.num_waves  = num_waves
     solver.num_eqn    = num_eqn
-    solver.weno_order = 5
+    solver.reconstruction_order = 5
+    solver.lim_type = 2
 
     solver.dt_variable = True
     solver.dt_initial  = dt/2.0
@@ -133,6 +151,8 @@ def em1D(mx=1024,num_frames=10,cfl=1.0,outdir='./_output',before_step=False,debu
     claw.solution = pyclaw.Solution(state,domain)
     claw.outdir = outdir
     claw.write_aux_always = True
+    claw.output_style = output_style
+    claw.num_output_times = 10
 
     return claw
 
