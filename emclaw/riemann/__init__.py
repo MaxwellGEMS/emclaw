0d4c64491e741c625807fd8bc584269eece2df4a
damiansa <damiansra@gmail.com>
2020-10-22 13:01:54 +0300

    updated __init__.py files for all modules

--- emclaw/riemann/__init__.py
+++ emclaw/riemann/__init__.py
@@ -1,4 +1,8 @@
-
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Wave propagation Riemann solvers implemented in Fortran.
+"""
 __pdoc__ = {
     'maxwell_1d':False,
     'maxwell_2d':False,
@@ -6,3 +10,16 @@ __pdoc__ = {
     'build': False,
     '__pycache__':False
 }
+
+from . import maxwell_1d_nc_rp
+from . import maxwell_1d_nc_tfluct
+from . import maxwell_1d_rp
+from . import maxwell_1d_tfluct
+
+from . import maxwell_2d_nc_rp
+from . import maxwell_2d_nc_tfluct
+from . import maxwell_2d_rp
+from . import maxwell_2d_tfluct
+
+from . import maxwell_3d_nl_rp as maxwell_3d_nc_rp
+from . import maxwell_3d_nl_tfluct as maxwell_3d_nc_tfluct
---
+++ emclaw/utils/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from . import basics
+from . import materials
+from . import sources
+from .io.inout import IO as io
